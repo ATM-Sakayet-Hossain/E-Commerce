1796b01d6ea38280fc23018314057c69c5dac442
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2025-08-29 01:53:41 +0600

    Detail Page

--- app/shop/[...id]/page.js
+++ app/shop/[...id]/page.js
@@ -5,6 +5,8 @@ import { Box, Rating } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function page ({ params }) {
   const { id } = params;
   const res = await fetch(`https://dummyjson.com/products/${id}`, {
